e8ace60c6bb0c329053c9953984f4993e2d22c06
Paulo Mitri <paulogdemitri@gmail.com>
2016-04-01 00:55:01 -0300

    login js improved. added angular. Get ready boys

--- js/login.js
+++ js/login.js
@@ -1,14 +1,18 @@
-var dialog = document.querySelector('dialog');
-var showDialogButton = document.querySelector('#signup');
-if (! dialog.showModal) {
-	dialogPolyfill.registerDialog(dialog);
-}
-showDialogButton.addEventListener('click', function() {
+function showDialog(){
+
+	var dialog = document.querySelector('dialog');
+	var showDialogButton = document.querySelector('#signup');
+	if (! dialog.showModal){
+		dialogPolyfill.registerDialog(dialog);
+	}
+
 	dialog.showModal();
-});
-dialog.querySelector('.close').addEventListener('click', function() {
+}
+
+function hideDialog(){
+	var dialog = document.querySelector('dialog');
 	dialog.close();
-});
+}
 
 $(document).ready(function(){
 		var bg_imgs = ["login1.jpg", "login2.jpg", "login3.jpg", "login4.jpg"];
