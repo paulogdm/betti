ab444116d73e602a5ed6ce7d364b24ddcb357044
Paulo Mitri <paulogdemitri@gmail.com>
2016-05-12 19:35:24 -0300

    profile icons inproved. Removed comment section
    CSS tweaks on sizes. Lots of improvements.

--- js/profile.js
+++ js/profile.js
@@ -6,7 +6,8 @@ angApp.controller('ProfileController', ['$scope', function($scope) {
 	$scope.profile_photo = './img/profile.jpg'; //substituir por uma funcao get
 	$scope.cover_photo = './img/cover.jpg';  
 	$scope.name = 'Steve';  
-	$scope.last_name = 'Wonder';  
+	$scope.last_name = 'Woggi';
+	$scope.description = 'User description go here, user description go here.';
 	
 }]);
 
@@ -15,18 +16,21 @@ angApp.controller('PostsController', ['$scope', function($scope) {
 
 	$scope.allPosts = [ 
 	{ 
-		title: 'TESTE1',
-		text: 'TESTE1'
+		title: 'Titulo1',
+		text: 'Lorem ipsum senectus habitant quisque litora scelerisque mollis massa primis himenaeos, hac id metus leo justo nam condimentum ullamcorper class aenean urna, morbi ligula ullamcorper fermentum duis tempus enim praesent quisque.',
+		date: new Date('2016', '05', '04', '3', '21'),
+		favorites: 99,
+		likes: 01,
+		forward: 02
 	}, 
 	{ 
-		title: 'TESTE2',
-		text: 'TESTE2'
-	} 
-	]
-
-	if($scope.allPosts)
-	{
-		
+		title: 'Titulo2',
+		text: 'Text2',
+		date: new Date('2016', '03', '08', '3', '55'),
+		favorites: 99,
+		likes: 01,
+		forward: 02
 	}
+	]
 
 }]);
