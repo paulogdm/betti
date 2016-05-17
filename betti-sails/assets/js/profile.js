d45c507ebb4ea092c73174d244fd226c22faf767
Paulo Mitri <paulogdemitri@gmail.com>
2016-05-17 20:48:46 -0300

    small changes

--- betti-sails/assets/js/profile.js
+++ betti-sails/assets/js/profile.js
@@ -1,35 +1,23 @@
+//LEVEL 2;
 
-var angApp = angular.module("betti-app", []);
-
-
-angApp.controller('SideBarController', ['$scope', function($scope) { 
-
-	$scope.user_name = 'Steve Woggi'; //substituir por uma funcao get
-	$scope.user_avatar = '/images/views/profile_default.png'; //substituir por uma funcao get
-}]);
-
-angApp.controller('ProfileController', ['$scope', function($scope) { 
+angular.module("betti-app").controller('ProfileController', ['$scope', function($scope) { 
 
 	$scope.profile_photo = '/images/views/profile_default.png'; //substituir por uma funcao get
 	$scope.cover_photo = '/images/views/cover_default.png';  
 	$scope.name = 'Steve';
 	$scope.last_name = 'Woggi';
 	$scope.description = 'User description go here, user description go here.';
-
-	var num = 0; //GET
-	$scope.color_mdl_class_profile = styleSwitch(num); //SYLE.JS
-
 }]);
 
-angApp.controller('StyleController', ['$scope', function($scope) { 
+angular.module("betti-app").controller('StyleController', ['$scope', function($scope) { 
 
 	var num = 0; // GET
-	$scope.color_mdl_class_page = styleSwitch(num); //STYLE.JS
+	$scope.color_mdl_class_profile = styleSwitch(num); //STYLE.JS
 
 }]);
 
 
-angApp.controller('PostsController', ['$scope', function($scope) { 
+angular.module("betti-app").controller('PostsController', ['$scope', function($scope) { 
 
 	$scope.allPosts = [ 
 	{ 
