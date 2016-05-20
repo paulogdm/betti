444bda1065a3affc5e4dbd428a814f77a421eb27
Paulo Mitri <paulogdemitri@gmail.com>
2016-05-20 08:58:49 -0300

    favorites added.

--- betti-sails/assets/js/profile.js
+++ betti-sails/assets/js/profile.js
@@ -50,6 +50,34 @@ angular.module("betti-app").controller('PostsController', ['$scope', function($s
 
 angular.module("betti-app").controller('PostActionController', ['$scope', function($scope){ 
 
+
+}]);
+
+angular.module("betti-app").controller('AllFavoritesController', ['$scope', function($scope){ 
+		$scope.allFavorites = [ 
+	{ 
+		title: 'Titulo1',
+		text: 'Lorem ipsum senectus habitant quisque litora scelerisque mollis massa primis himenaeos, hac id metus leo justo nam condimentum ullamcorper class aenean urna, morbi ligula ullamcorper fermentum duis tempus enim praesent quisque.',
+		date: new Date('2016', '05', '04', '3', '21'),
+		favorites: 99,
+		likes: 01,
+		forward: 02,
+		liked: false,
+		favorited: true,
+		shared: false
+	}, 
+	{ 
+		title: 'Titulo2',
+		text: 'Text2',
+		date: new Date('2016', '03', '08', '3', '55'),
+		favorites: 99,
+		likes: 01,
+		forward: 02,
+		liked: true,
+		favorited: false,
+		shared: true
+	}
+	]
 }]);
 
 angular.module("betti-app").controller('AllFriendsController', ['$scope', function($scope){ 
