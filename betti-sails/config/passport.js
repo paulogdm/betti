c131ad899f7804c2e1d15064bd551d0b33d75516
Paulo Mitri <paulogdemitri@gmail.com>
2016-05-23 22:46:56 -0300

    passport local strategy not being called.

--- betti-sails/config/passport.js
+++ betti-sails/config/passport.js
@@ -2,31 +2,38 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 passport.use(new LocalStrategy(
-	function(login, password, done) {
-		User.find({'login': login}).exec(function(err, user) {
+    function(username, password, done) {
+        User.find({login: username}).exec(function(err, user) {
 
-			if (err) {
-				console.error('ERR FETCH'); // #sublime save location
-				return done(null, err);
-			}
-			if (!user || user.length != 1) {
-				return done(null, false, {message: 'Incorrect User'});
-			}
+			sails.log.error("WHY THIS IS NOT WORKING????");
 
-			if( password !== user[0].password ){
-				return done(null, false, { message: 'Invalid Password' });
-			} else {
-				return done(null,user);
-			}
-		});
-	})
+            if (err) {
+                return done(null, err);
+            }
+            if (!user || user.length < 1) {
+                return done(null, false, {
+                    message: 'Incorrect User'
+                });
+            }
+
+            if ( password === user[0].password, function(err, res) {
+                if (err || !res) {
+                    return done(null, false, {
+                        message: 'Invalid Password'
+                    });
+                } else {
+                    return done(null,user);
+                }
+            });
+        });
+    })
 );
 
 module.exports = {
-	http: {
-		customMiddleware: function(app) {
-			app.use(passport.initialize());
-			app.use(passport.session());
-		}
-	}
+    http: {
+        customMiddleware: function(app) {
+            app.use(passport.initialize());
+            app.use(passport.session());
+        }
+    }
 };
