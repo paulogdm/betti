809917e5c4637e67e783b1e577094257f1b78a7a
Paulo Mitri <paulogdemitri@gmail.com>
2016-05-23 22:46:56 -0300

    passport local strategy not being called.

--- betti-sails/config/routes.js
+++ betti-sails/config/routes.js
@@ -3,14 +3,5 @@ module.exports.routes = {
 
   '/': {
     view: 'login'
-
-  },
-  'get /user/login':{
-    controller: 'user',
-    action: 'login'
-  },
-  'post /user/login':{
-    controller: 'user',
-    action: 'passport_local'
   }
 };
