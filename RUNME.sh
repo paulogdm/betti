589d7817ef243aa4e81e93548fd64b462260f262
Paulo Mitri <paulogdemitri@gmail.com>
2016-06-04 15:01:24 -0300

    duh, only one shell script needed.

--- RUNME.sh
+++ RUNME.sh
@@ -1,8 +1,17 @@
-cd betti-sails/assets
+# Running this project for the first time?
+# Then run this sh to save your time.
+cd betti-sails
+sudo npm install
+sudo npm install -g bower
+
+cd assets
 bower install
 
 
 # THIS FILE IS VERY IMPORTANT!
+# What is it? 	Custom express=jwt policies to work with 
+# 				cookies instead of headers (much harder)
 # place it in node_modules/express-jwt/lib/index.js
 cd ../
 mv ./index.js ./node_modules/express-jwt/lib/index.js
+
--- RUNME_SUDO.sh
+++
@@ -1,5 +0,0 @@
-# Running this project for the first time?
-# Then run this sh to save your time.
-cd betti-sails
-npm install
-npm install -g bower
