8e2f504c57338345f5df06e72b0bade9d5acdb73
Paulo Mitri <paulogdemitri@gmail.com>
2016-06-06 10:30:16 -0300

    connections updated

--- betti-sails/config/connections.js
+++ betti-sails/config/connections.js
@@ -13,8 +13,8 @@ module.exports.connections = {
     host: '198.199.79.4',
     port: '5432',
     user: 'pguser', // optional
-    password: ' EDIT HERE', // optional
-    database: 'db_betti_1' //optional
+    password: 'EDIT', // optional
+    database: 'webdb' //optional
   }
 
 };
