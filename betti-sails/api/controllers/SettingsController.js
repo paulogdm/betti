f484ea8959cdd4b3c37eac2e010b8560e481e8ae
Paulo Mitri <paulogdemitri@gmail.com>
2016-06-05 21:17:05 -0300

    file upload draft

--- betti-sails/api/controllers/SettingsController.js
+++ betti-sails/api/controllers/SettingsController.js
@@ -5,7 +5,38 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-module.exports = {
-	
-};
+ module.exports = {
+ 	
+ 	uploadAvatar: function (req, res){
+
+		var requester = req.cookies.token;
+		
+		AuthService.tokendecode(requester, function(data){
+			
+			if(!data.success){
+				return res.json({success: false});
+			} else {
+				requester = data.user.login.trim();
+
+				req.file('avatar').upload({
+						maxBytes: 2000000, //2MB
+						dirname: require('path').resolve(sails.config.appPath, '/assets/images/users'),
+						saveAs: 'avatar_'+requester+'.jpg'
+					},function whenDone(err, file){
+			
+					if (err){
+						return res.json({success: false, err: err});
+					}
+
+					// If no files were uploaded, respond with an error.
+					if (file.length === 0){
+						return res.json({success: false});
+					}
+
+					return res.json({success: true, url: '/images/users/avatar_'+requester+'.jpg'});
+				});
+			}
+		});
+ 	}
+ };
 
