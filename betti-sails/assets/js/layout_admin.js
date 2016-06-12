a127b06c5e79d9caa68336ff8b90594baa8880dc
Paulo Mitri <paulogdemitri@gmail.com>
2016-06-12 09:28:39 -0300

    preparing admin layout

--- betti-sails/assets/js/layout_admin.js
+++ betti-sails/assets/js/layout_admin.js
@@ -1,13 +1,178 @@
 var angApp = angular.module("betti-app", []);
 
-angApp.controller('SideBarController', ['$scope', function($scope) { 
+angApp.factory('GetLayoutService', function($http) {
+	return {
+		'get_profile': function(data){
+			return $http.post('/profile/getprofile/', data);
+		}
+	}
+});
 
-	$scope.user_name = 'Badmin'; //substituir por uma funcao get
-	$scope.user_avatar = '/images/views/admin_default.png'; //substituir por uma funcao get
+
+angApp.factory('LogoutService', function($http) {
+	return {
+		'logout': function(data){
+			return $http.delete('/auth/logout', data);
+		}
+	}
+});
+
+angApp.factory('LayoutStyleService',['GetLayoutService',
+ function(GetLayoutService) {
+	
+	var color_search = 0;
+	var color_mdl_search = styleSwitch(0);
+	var color_profile = 0;
+	var color_mdl_profile = styleSwitch(0);
+
+	var avatar = '';
+	var birthday = '';
+	var name = '';
+	var cover = '';
+	var login = '';
+
+	var LayoutStyleService = {};
+
+	LayoutStyleService.request = function(value){
+		GetLayoutService.get_profile().then(
+			function(response){
+				if(response.status == 200){
+					
+					LayoutStyleService.setAvatar(response.data.uphoto);
+					LayoutStyleService.setCover(response.data.cover);
+					LayoutStyleService.setBirthday(response.data.birthday);
+					LayoutStyleService.setName(response.data.uname.trim());
+					LayoutStyleService.setLogin(response.data.login.trim());
+
+					LayoutStyleService.setColorSearch(response.data.style_bar);
+					LayoutStyleService.setColorProfile(response.data.style_profile);
+				}
+			},function(response) {
+				console.info("[Layout][get_profile] Error received!");
+			}
+		);
+	}
+
+	LayoutStyleService.setColorSearch = function(value){
+		this.color_search = value; //STYLE.JS
+		this.color_mdl_search = styleSwitch(value);
+	}
+
+	LayoutStyleService.getColorSearch = function(){
+		return color_search;
+	}
+
+	LayoutStyleService.getColorMdlSearch = function(){
+		return this.color_mdl_search;
+	}
+	
+	LayoutStyleService.setColorProfile = function(value){
+		this.color_profile = value; //STYLE.JS
+		this.color_mdl_profile = styleSwitch(value);
+	}
+
+	LayoutStyleService.getColorProfile = function(){
+		return color_profile;
+	}
+
+	LayoutStyleService.getColorMdlProfile = function(){
+		return this.color_mdl_profile;
+	}
+
+	LayoutStyleService.setName = function(value){
+		this.name = value;
+	}
+
+	LayoutStyleService.getName = function(){
+		return this.name;
+	}
+
+	LayoutStyleService.setLogin = function(value){
+		this.login = value;
+	}
+
+	LayoutStyleService.getLogin = function(){
+		return this.login;
+	}
+	
+	LayoutStyleService.setBirthday = function(value){
+		this.birthday = value;
+	}
+
+	LayoutStyleService.getBirthday = function(){
+		return this.birthday;
+	}
+
+	LayoutStyleService.setAvatar = function(value){
+		this.avatar = value;
+	}
+
+	LayoutStyleService.getAvatar = function(){
+		return this.avatar;
+	}
+
+	LayoutStyleService.setCover = function(value){
+		this.cover = value;
+	}
+
+	LayoutStyleService.getCover = function(){
+		return this.cover;
+	}
+
+	return LayoutStyleService;
+}]);
+
+
+angApp.controller('SideBarController', ['$scope', 'LayoutStyleService', 'GetLayoutService',
+	'LogoutService',
+	function($scope, LayoutStyleService, GetLayoutService, LogoutService){ 
+
+	LayoutStyleService.request();
+
+	$scope.avatar = LayoutStyleService.getAvatar();
+	$scope.birthday = LayoutStyleService.getBirthday();
+	$scope.name = LayoutStyleService.getName();
+	$scope.login = LayoutStyleService.getLogin();
+
+	$scope.color_mdl_search = LayoutStyleService.getColorMdlSearch();
+	$scope.color_mdl_profile = LayoutStyleService.getColorMdlProfile();
+
+
+	$scope.$watch(
+		function(){
+			$scope.color_mdl_profile = LayoutStyleService.getColorMdlProfile();
+			$scope.color_mdl_search = LayoutStyleService.getColorMdlSearch();
+
+			$scope.avatar = LayoutStyleService.getAvatar();
+			$scope.birthday = LayoutStyleService.getBirthday();
+			$scope.name = LayoutStyleService.getName();
+			$scope.login = LayoutStyleService.getLogin();
+		}
+	)
+
+	$scope.logout = function(){
+		LogoutService.logout().then(
+		function(response){
+			if(response.status == 200){
+				var now = new Date();
+
+				var now = new Date();
+
+				document.cookie = 'token=; expires=Thu, 01 Jan 1993 00:00:01 GMT; Path=/;';
+				window.location.href = '/';
+			}
+		},function(response) {
+			console.info("[Layout][get_profile] Error received!");
+		}
+	);
+	}
 
 }]);
 
-angApp.controller('LayoutStyleController', ['$scope', function($scope) { 
-	var num = 6; // GET
-	$scope.color_mdl_class_page = styleSwitch(num); //STYLE.JS
-}]);
+function showSnackbar(msg){ 
+	var notification = document.querySelector('#snackbar-show'); 
+	var data = {
+		message: msg,
+	};
+	notification.MaterialSnackbar.showSnackbar(data); 
+}
--- betti-sails/assets/js/login.js
+++ betti-sails/assets/js/login.js
@@ -52,70 +52,6 @@ angApp.factory('UserService', function($http) {
 	}
 });
 
-angApp.controller('MainController', ['$scope', 'UserService', function($scope, UserService) { 
-
-	angular.isUndefinedOrNull = function(val) {
-		return angular.isUndefined(val) || val === null 
-	}
-
-	$scope.submitLogin = function (){
-
-		var msgSuc = "Welcome again!";
-		var msgErr = "Error received from server. Sorry!";
-		var msgFail = "Invalid credentials.";
-		var flag = false;
-
-		
-		if (
-			angular.isUndefinedOrNull($scope.user)					|| 
-			angular.isUndefinedOrNull($scope.user.login)			|| 
-			angular.isUndefinedOrNull($scope.user.password)
-			){
-			showSnackbar(msgErr+" Check your information please...");
-
-	} else {
-		var login = $scope.user.login;
-		var password = $scope.user.password;
-
-		var data = {
-			login: login,
-			password: password
-		}
-
-		UserService.login(data).then(
-			function(response) {
-				if(response.data.success){
-					console.info("[Login] Success!!!");
-
-					var now = new Date();
-					var time = now.getTime();
-						time += 20*60*60*1000; //20h
-						now.setTime(time);
-						document.cookie = "token="+response.data.token+
-						'; expires=' + now.toUTCString() + '; Path=/;';
-
-
-						console.info(response.data);
-						showSnackbar(msgSuc);
-
-						if(data.login == 'admin') 
-							window.location.href = '/adminspace/adminpanel/';
-						else 
-							window.location.href = '/userspace/profile/'+data.login;
-
-				} else {
-					console.info("[Login] Failed!");
-					showSnackbar(msgFail);
-				}
-
-			},
-			function(response) {
-				showSnackbar(msgErr);
-				console.info("[Login] Error received!");
-			}
-		);
-	}
-}
 
 function validDate(text) {
 
@@ -140,54 +76,120 @@ function validDate(text) {
 		date.getDate() == d && date.getTime() < now.getTime());
 }
 
+angApp.controller('MainController', ['$scope', 'UserService', function($scope, UserService) { 
 
-$scope.submitNewLogin = function (){
-
-	var msgSuc = "Welcome! Now you can try your brand new login!"; 
-	var msgErr = "Sorry! Something is not right.";
-
-
-	if (
-		angular.isUndefinedOrNull($scope.user)					|| 
-		angular.isUndefinedOrNull($scope.user.new_login)		|| 
-		angular.isUndefinedOrNull($scope.user.new_password)		|| 
-		angular.isUndefinedOrNull($scope.user.new_name)			|| 
-		angular.isUndefinedOrNull($scope.user.new_date) 
-		){
-		showSnackbar(msgErr+" Check your information please...");
-
-	} else {
-		var newLogin = $scope.user.new_login;
-		var newPass = $scope.user.new_password;
-		var newName = $scope.user.new_name;
-		var newBirth = $scope.user.new_date;
-
-		if(!validDate(newBirth)){
-			showSnackbar("'"+newBirth+"' is not a valid date...");
-			return;
-		}
-
-		var data = {
-			login: newLogin,
-			password: newPass,
-			uname: newName,
-			birthday: newBirth
-		};
-
-		UserService.create(data).then(
-			function(response) {
-				showSnackbar(msgSuc);
-				console.info("[Signup] Success!!!");
-				console.info(response.data);
-				hideDialog();
-			},
-			function(response) {
-				showSnackbar(msgErr);
-				console.info("[Signup] Error received!");
-			}
-		);
+	angular.isUndefinedOrNull = function(val) {
+		return angular.isUndefined(val) || val === null 
 	}
-]});
+
+	$scope.submitLogin = function (){
+
+		var msgSuc = "Welcome again!";
+		var msgErr = "Error received from server. Sorry!";
+		var msgFail = "Invalid credentials.";
+		var flag = false;
+
+		
+		if (
+			angular.isUndefinedOrNull($scope.user)					|| 
+			angular.isUndefinedOrNull($scope.user.login)			|| 
+			angular.isUndefinedOrNull($scope.user.password)
+			){
+			showSnackbar(msgErr+" Check your information please...");
+
+		} else {
+			var login = $scope.user.login;
+			var password = $scope.user.password;
+
+			var data = {
+				login: login,
+				password: password
+			}
+
+			UserService.login(data).then(
+				function(response) {
+					if(response.data.success){
+						console.info("[Login] Success!!!");
+
+						var now = new Date();
+						var time = now.getTime();
+							time += 20*60*60*1000; //20h
+							now.setTime(time);
+							document.cookie = "token="+response.data.token+
+							'; expires=' + now.toUTCString() + '; Path=/;';
+
+
+							console.info(response.data);
+							showSnackbar(msgSuc);
+
+							if(data.login == 'admin') 
+								window.location.href = '/adminspace/adminpanel/';
+							else 
+								window.location.href = '/userspace/profile/'+data.login;
+
+					} else {
+						console.info("[Login] Failed!");
+						showSnackbar(msgFail);
+					}
+
+				},
+				function(response) {
+					showSnackbar(msgErr);
+					console.info("[Login] Error received!");
+				}
+			);
+		}
+	}
+
+
+	$scope.submitNewLogin = function (){
+
+		var msgSuc = "Welcome! Now you can try your brand new login!"; 
+		var msgErr = "Sorry! Something is not right.";
+
+
+		if (
+			angular.isUndefinedOrNull($scope.user)					|| 
+			angular.isUndefinedOrNull($scope.user.new_login)		|| 
+			angular.isUndefinedOrNull($scope.user.new_password)		|| 
+			angular.isUndefinedOrNull($scope.user.new_name)			|| 
+			angular.isUndefinedOrNull($scope.user.new_date) 
+			){
+			showSnackbar(msgErr+" Check your information please...");
+
+		} else {
+			var newLogin = $scope.user.new_login;
+			var newPass = $scope.user.new_password;
+			var newName = $scope.user.new_name;
+			var newBirth = $scope.user.new_date;
+
+			if(!validDate(newBirth)){
+				showSnackbar("'"+newBirth+"' is not a valid date...");
+			} else {
+
+				var data = {
+					login: newLogin,
+					password: newPass,
+					uname: newName,
+					birthday: newBirth
+				};
+
+				UserService.create(data).then(
+					function(response) {
+						showSnackbar(msgSuc);
+						console.info("[Signup] Success!!!");
+						console.info(response.data);
+						hideDialog();
+					},
+					function(response) {
+						showSnackbar(msgErr);
+						console.info("[Signup] Error received!");
+					}
+				);
+			}
+		}
+	}
+}]);
 
 angApp.controller('BGController', ['$scope', function($scope) { 
 	var imgCount = 4; //number of images
